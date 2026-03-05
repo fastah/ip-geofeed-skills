ea06fc5488e971c605975ad0f02460685529c6fe
Punit <punit@getfastah.com>
2026-03-05 14:13:23 +0530

    [#16] Add limit entries

--- experimental/ip-geofeed/main.go
+++ experimental/ip-geofeed/main.go
@@ -10,25 +10,27 @@ import (
 func main() {
 
 	bulk := flag.Bool("bulk", false, "Enable bulk validation mode")
+	limitEntries := flag.Int("limit-entries", 0, "Limit the number of entries to validate (0 = no limit)")
 	flag.Parse()
 
 	if flag.NArg() < 1 {
 		fmt.Println("Usage:")
 		fmt.Println("  geofeed-validator <csv-file-or-url>")
 		fmt.Println("  geofeed-validator --bulk <file-with-urls>")
+		fmt.Println("  geofeed-validator --limit-entries <number> <csv-file-or-url>")
 		os.Exit(1)
 	}
 
 	input := flag.Arg(0)
 
 	if *bulk {
-		err := geofeed.GeofeedsValidation(input)
+		err := geofeed.GeofeedsValidation(input, *limitEntries)
 		if err != nil {
 			fmt.Printf("Error validating geofeeds: %v\n", err)
 			os.Exit(1)
 		}
 	} else {
-		err := geofeed.GeofeedValidation(input)
+		err := geofeed.GeofeedValidation(input, *limitEntries)
 		if err != nil {
 			fmt.Printf("Error validating geofeed: %v\n", err)
 			os.Exit(1)
